a3f50cb06f06a083665e14a3909c9a8cee172edc
Titobioluwa Dada <Titobioluwad@gmail.com>
2026-02-11 00:55:59 -0330

    used data source to get ID of AMI's for different regions

---
+++ data-sources-proj/compute.tf
@@ -0,0 +1,44 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-*-20.04-amd64-server-*"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+data "aws_ami" "ubuntu-us-east" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical
+  provider = aws.us-east
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-*-20.04-amd64-server-*"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+output "ubuntu-ami-value" {
+  value = data.aws_ami.ubuntu.id
+}
+output "ubuntu-us-east-ami-value" {
+  value = data.aws_ami.ubuntu-us-east.id
+}
+
+# resource "aws_instance" "ubuntu-instance" {
+#     ami           = data.aws_ami.ubuntu.id
+#     instance_type = t2.micro
+#   root_block_device {
+#     volume_size = 10
+#     volume_type = gp3
+#     delete_on_termination = true
+#   }
+# }
