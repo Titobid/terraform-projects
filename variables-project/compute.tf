a3c339844a562b546d02d902bddec4efc18e388f
Titobioluwa Dada <Titobioluwad@gmail.com>
2026-02-20 20:52:56 -0330

    added tags

--- variables-project/compute.tf
+++ variables-project/compute.tf
@@ -1,15 +1,23 @@
+locals {
+  common_tags = {
+    project       = "terraform-projects"
+    project_owner = "Titobioluwa"
+    cost_center   = "prod-terraform-projects"
+    managed_by    = "terraform"
+  }
+}
 data "aws_ami" "ubuntu" {
-    most_recent = true
-    owners = ["099720109477"] # Canonical
+  most_recent = true
+  owners      = ["099720109477"] # Canonical
 
   filter {
     name   = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-*-20.04-amd64-server-*"]
   }
   filter {
-  name = "virtualization-type"
-  values = ["hvm"]
-}
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
 }
 
 output "ubuntu-ami-id" {
@@ -17,12 +25,13 @@ output "ubuntu-ami-id" {
 }
 
 resource "aws_instance" "ubuntu-instance" {
-  ami = data.aws_ami.ubuntu.id
+  tags          = local.common_tags
+  ami           = data.aws_ami.ubuntu.id
   instance_type = var.ec2_instance_type
   root_block_device {
     delete_on_termination = true
-    volume_type = var.ec2_volume_config.type
-    volume_size = var.ec2_volume_config.size
-}
+    volume_type           = var.ec2_volume_config.type
+    volume_size           = var.ec2_volume_config.size
+  }
 }
 
