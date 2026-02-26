c76a2a23424d74b1dd9ac63dd2daf375af9b86d8
Titobioluwa Dada <Titobioluwad@gmail.com>
2026-02-25 23:07:19 -0330

    using count to create multiple subnets with unique cidr blockss

---
+++ mulitple-resources/networking.tf
@@ -0,0 +1,20 @@
+locals {
+  project = "multiple-resource"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    project = local.project
+    Name    = local.project
+  }
+}
+
+resource "aws_subnet" "main" {
+  count      = var.subnet_count
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.${count.index}.0/24"
+  tags = {
+    Name = "${local.project}-${count.index}"
+  }
+}
