2d1378d1cab024f4e426a624139a8a2b43382593
Titobioluwa Dada <Titobioluwad@gmail.com>
2026-03-02 22:18:33 -0330

    added validation for instance_type and ami

--- mulitple-resources/variables.tf
+++ mulitple-resources/variables.tf
@@ -9,4 +9,12 @@ variable "ec2_instance_config_list" {
     instance_type = string
     ami           = string
   }))
+  validation {
+    condition = alltrue([for config in var.ec2_instance_config_list : contains(["ubuntu", "nginx"], config.ami)])
+    error_message = "only supports 'ubuntu' and 'nginx' as AMI values"
+  }
+  validation {
+    condition = alltrue([for config in var.ec2_instance_config_list : contains(["t2.micro"], config.instance_type)])
+    error_message = "only supports 't2.micro' as instance type"
+  }
 }
